0c14788f0b52f0d5b49549c7d4de0b5da0516730
Jaime Burbano <burbanojaime98@gmail.com>
2023-11-28 13:11:57 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,1 +1,10 @@
-   function skillsMember() 
+   function skillsMember() {
+      var skills = document.getElementById("skills");
+      var member = document.getElementById("member");
+      var memberValue = member.options[member.selectedIndex].value;
+      if (memberValue == "0") {
+         skills.style.display = "none";
+      } else {
+         skills.style.display = "block";
+      }
+   }
